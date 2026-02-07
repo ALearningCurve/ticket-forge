587759bf9bb1e33387118e3f5cb29515dc9a081f
Sameer Saxena <sameersaxena2810@gmail.com>
2026-02-07 18:46:02 -0500

    [#8] fix: address linting issues in embedding service

--- libs/ml-core/ml_core/embeddings/__init__.py
+++ libs/ml-core/ml_core/embeddings/__init__.py
@@ -1,5 +1,5 @@
 """Embedding services for text vectorization."""
 
-from .service import EmbeddingService, get_embedding_service
+from ml_core.embeddings.service import EmbeddingService, get_embedding_service
 
-__all__ = ['EmbeddingService', 'get_embedding_service']
+__all__ = ["EmbeddingService", "get_embedding_service"]
